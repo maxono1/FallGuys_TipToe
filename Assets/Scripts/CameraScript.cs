66f4b8375389d0e8e213ce4361bcbc8540ae7e3e
maxono1 <92861475+maxono1@users.noreply.github.com>
2022-10-11 11:15:34 +0200

    lesgoooo

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,14 +1,13 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SimpleCharacterControl : MonoBehaviour
+public class CameraScript : MonoBehaviour
 {
-    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
