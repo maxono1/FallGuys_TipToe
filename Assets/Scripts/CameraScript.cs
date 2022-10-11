7fed0c0746737fc1b24155ecd213a18bb3d6628f
maxono1 <92861475+maxono1@users.noreply.github.com>
2022-10-11 11:32:49 +0200

    camera pos

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public GameObject player;
+
+    private Vector3 offset;
+
     void Start()
     {
         
@@ -13,6 +18,9 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = player.transform.position - new Vector3(0, -3, 8);
+        //hier noch mousex
+        //Input.GetAxis("Mouse X");
+        //transform.rotation
     }
 }
