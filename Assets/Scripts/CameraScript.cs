e605dcffbe99ef7057662da98fd4a153fb21cf38
maxono1 <92861475+maxono1@users.noreply.github.com>
2022-10-12 23:30:59 +0200

    aufg1 und 2 fertig

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,23 +4,63 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 cameraOffset;
+    [SerializeField] private float verticalOffset;
+    [SerializeField] private Vector3 distance;
+    [SerializeField] private float rotationSpeed = 100.0f;
+    [SerializeField] [Range(0.01f, 1.0f)] private float smoothness = 0.5f;
+    [SerializeField] private float minFov = 40.0f;
+    [SerializeField] private float maxFov = 60.0f;
 
-    public GameObject player;
 
-    private Vector3 offset;
-
-    void Start()
+    private void Start()
     {
-        
+        cameraOffset = this.target.position + distance;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        transform.position = player.transform.position - new Vector3(0, -3, 8);
-        //hier noch mousex
-        //Input.GetAxis("Mouse X");
-        //transform.rotation
+       
     }
+
+
+    private void LateUpdate()
+    {
+        this.ZoomCamera();
+        this.OrbitCamera();
+    }
+
+
+    private void ZoomCamera()
+    {
+        // Jeden Frame checken ob der Nutzer zoomt
+        float camFov = GetComponent<Camera>().fieldOfView;
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && camFov > minFov)
+        {
+            this.GetComponent<Camera>().fieldOfView--;
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0 && camFov < maxFov)
+        {
+            this.GetComponent<Camera>().fieldOfView++;
+        }
+    }
+
+    private void OrbitCamera()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime, Vector3.up);
+            cameraOffset = camTurnAngle * cameraOffset;
+        }
+
+        Vector3 newPos = this.target.position + cameraOffset;
+        // Sphere Interpolation damit Kamera nicen übergang hat und nicht ruckartig wie ne missgeburt stoppt. 0.01 = nice smooth, 1.00 = missgeburt stop
+        this.transform.position = newPos; //Vector3.Slerp(this.transform.position, newPos, smoothness);
+        this.transform.LookAt(new Vector3(this.target.position.x, this.target.position.y + verticalOffset, this.target.position.z));
+    }
+    
+   
 }
--- Assets/Scripts/SimpleCharacterControl.cs
+++ Assets/Scripts/SimpleCharacterControl.cs
@@ -5,15 +5,101 @@ using UnityEngine;
 public class SimpleCharacterControl : MonoBehaviour
 {
     private Animator animator;
+    public Rigidbody rb;
+    public Transform cameraTransform;
     // Start is called before the first frame update
+
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float acceleration;
+    private Vector3 currentVelocity;
+
     void Start()
     {
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        currentVelocity = new(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveCharacter();
+        rotateCharacter();
+    }
+
+
+    private void rotateCharacter()
+    {
+        if(currentVelocity.sqrMagnitude > 0.1f)
+        {
+            rb.transform.LookAt(transform.position + currentVelocity);
+
+        }
+    }
+
+    private void moveCharacter()
+    {
+        //to add together the sideways movements, have a direction vector where i just add to it
+        //the fall guy should turn into this direction with soome sort of interpolation, but he immediately moves there???
+        Vector3 movementDirection = new Vector3(0, 0, 0);
+        Vector3 fwdMovement = new Vector3(cameraTransform.forward.x, 0, cameraTransform.forward.z);
+        Vector3 rightMovement = new Vector3(cameraTransform.right.x, 0, cameraTransform.right.z);
+
+        if (Input.GetKey("w") && !Input.GetKey("s"))
+        {
+            //print("move forward");
+            movementDirection += fwdMovement * maxSpeed;
+
+        }
+        else if (Input.GetKey("s") && !Input.GetKey("w"))
+        {
+            //print("move back");
+            movementDirection -= fwdMovement * maxSpeed;
+        }
+
+        if (Input.GetKey("a") && !Input.GetKey("d"))
+        {
+            movementDirection -= rightMovement * maxSpeed;
+        }
+        else if (Input.GetKey("d") && !Input.GetKey("a"))
+        {
+            movementDirection += rightMovement * maxSpeed;
+            //print("move right");
+        }
+
+
+        currentVelocity = movementDirection * maxSpeed;
+        rb.MovePosition(rb.position + currentVelocity * Time.deltaTime);
+
+
+
+        //currentVelocity += acceleration * Time.deltaTime * movementDirection;
+        /*
+        if(currentVelocity.x > maxSpeed)
+        {
+            currentVelocity.x = maxSpeed;
+        } 
+        else if (currentVelocity.x < -maxSpeed)
+        {
+            currentVelocity.x = -maxSpeed;
+        }
+
+        if (currentVelocity.y > maxSpeed)
+        {
+            currentVelocity.y = maxSpeed;
+        }
+        else if (currentVelocity.y < -maxSpeed)
+        {
+            currentVelocity.y = -maxSpeed;
+        }
+
+        if (currentVelocity.z > maxSpeed)
+        {
+            currentVelocity.z = maxSpeed;
+        }
+        else if (currentVelocity.z < -maxSpeed)
+        {
+            currentVelocity.z = -maxSpeed;
+        }*/
     }
 }
